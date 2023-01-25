5f333715e19a4c4aef62665b53a358dcb0997c9d
Adam-Robson <adamray312@gmail.com>
2023-01-25 15:07:37 -0800

    fix colors change to home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
-import Colors from './components/Colors/Colors';
+import Home from './components/Home/Home';
 import Color from './components/Color/Color';
 import NotFound from './components/NotFound/NotFound.jsx';
 
